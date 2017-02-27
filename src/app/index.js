0da6c78143190defc04910be297314024740b82f
Gaurav Chauriya <gaurav.chauriya@cuelogic.com>
2017-02-27 13:46:08 +0530

    Using State and checking how DOM renders in React

--- src/app/index.js
+++ src/app/index.js
@@ -17,7 +17,7 @@ class App extends React.Component {
         </div>
         <div className="row">
           <div className="col-xs-10 col-xs-offset-1">
-            <Home age={27} name={"Josh"} />
+            <Home initialAge={27} name={"Josh"} />
           </div>
         </div>
       </div>
